ab350d4fba70a9a53852b29bcabc888e24b03f44
C4RC0 <karesz.carco@gmail.com>
2021-10-01 22:56:18 +0200

    initial release

--- packages/hatchet-on-the-table/src/common/config/constants/routes.js
+++ packages/hatchet-on-the-table/src/common/config/constants/routes.js
@@ -2,7 +2,7 @@ const routes = {
     conceptRoute: "/concept",
     canvasRoute: "/canvas",
     gitHubUrl: "https://github.com/C4RC0/hatchet-on-the-table",
-    behanceUrl: "https://www.behance.net/gallery/127631653/hatchet-on-the-table",
+    behanceUrl: "https://www.behance.net/gallery/128515397/Hatchet-on-the-table",
 };
 
 export default routes;
